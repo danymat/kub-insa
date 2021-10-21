eb52c9748bd91188a21ca9d4bfdc97598bbe0fbe
Daniel Mathiot <d.danymat@gmail.com>
2021-10-21 19:30:29 +0200

    Do not create messages on GET endpoint

--- services/backend/index.js
+++ services/backend/index.js
@@ -1,51 +1,49 @@
 const express = require('express')
 const cors = require('cors')
+const redis = require('redis')
+
 const app = express()
 const port = process.env.PORT || 3000
-const redis = require('redis')
 app.use(cors())
 
 app.use(
-  express.urlencoded({
-    extended: true
-  })
+    express.urlencoded({
+        extended: true
+    })
 )
 
 app.use(express.json())
 
 const client = redis.createClient({
-  socket: {host:'172.18.0.2'}
+    socket: { host: '172.18.0.2' }
 })
 
 client.on('error', (err) => {
-  console.log("Error connecting to database", err)
-  //process.exit(1)
+    console.log("Error connecting to database", err)
+    process.exit(1)
 })
 
 client.connect().then(() => {
-  app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+    app.get('/', (_, res) => {
+        res.send('Hello World!')
+    })
 
-  app.post('/messages', async (req, res) => {
-    console.log(req)
-    console.log(req.body)
-    let message = req.body.message
-    await client.hSet('messages', '10', message)
-    res.send("Message dans la DB")
-  })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
-  
-  app.get('/messages', async (req, res) => {
-    await client.hSet('messages', '1', 'Bonjour')
-    await client.hSet('messages', '2', 'Bonjour')
-    await client.hSet('messages', '3', 'Bonjour')
-    let messages = await client.hGetAll('messages')
-    res.send(messages)
-  })
+    app.post('/messages', async (req, res) => {
+        console.log(req)
+        console.log(req.body)
+        let message = req.body.message
+        await client.hSet('messages', 'key', message)
+        res.send("Message dans la DB")
+    })
+
+    app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`)
+    })
+
+    app.get('/messages', async (_, res) => {
+        let messages = await client.hGetAll('messages')
+        res.send(messages)
+    })
 })
 
 
