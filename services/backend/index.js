1cf9aa5550dd2e9a8e20fea6585ae5252daebc78
ColinPBL <colinpoubel@orange.fr>
2021-10-21 14:25:41 +0200

    Created link to database in backend

--- services/backend/index.js
+++ services/backend/index.js
@@ -1,11 +1,36 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const redis = require('redis')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const client = redis.createClient({
+  socket: {host:'172.18.0.2'}
+})
+console.log(client)
+
+client.on('error', (err) => {
+  console.log("Error connecting to database", err)
+  //process.exit(1)
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+client.connect().then(() => {
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
+  
+  app.get('/messages', async (req, res) => {
+    await client.hSet('messages', '1', 'Bonjour')
+    await client.hSet('messages', '2', 'Bonjour')
+    await client.hSet('messages', '3', 'Bonjour')
+    let messages = await client.hGetAll('messages')
+    res.send(messages)
+  })
 })
+
+
+
+
